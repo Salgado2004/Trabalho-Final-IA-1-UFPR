757c83251ed6e8c373583083560c01021de76708
salgado2004 <leosalgado2004@gmail.com>
2025-05-26 15:06:48 -0300

    Refactor image processing functions and move utility functions to utils.py

--- teste.py
+++ teste.py
@@ -1,17 +1,7 @@
 import cv2
 import numpy as np
 from joblib import load
-
-IMAGES = [
-    "./data/bom_1.jpg",
-    "./data/bom_2.jpg",
-    "./data/bom_3.jpg",
-    "./data/bom_4.jpg",
-    "./data/ruim_1.jpg",
-    "./data/ruim_2.jpg",
-    "./data/ruim_3.jpg",
-    "./data/ruim_4.jpg"
-]
+from utils import apply_sobel, IMAGES
 
 def main():
     while True:
@@ -40,15 +30,6 @@ def main():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         print("\033[H\033[J", end="")
-
-def apply_sobel(img):
-    img_gray = img
-    grad_x = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
-    grad_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-    img_sobel = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-    return img_sobel
 
 def prepare_data(image_path):
     image = cv2.imread(image_path)
--- treinamento.py
+++ treinamento.py
@@ -4,20 +4,10 @@ import time
 import numpy as np
 from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
-from joblib import dump, load
+from joblib import dump
+from utils import apply_sobel, IMAGES
 
 # Classe 1 - Bom, 0 - Ruim
-IMAGES = [
-    { 'path': "./data/bom_1.jpg", 'classe': 1},
-    { 'path': "./data/bom_2.jpg", 'classe': 1},
-    { 'path': "./data/bom_3.jpg", 'classe': 1},
-    { 'path': "./data/bom_4.jpg", 'classe': 1},
-    { 'path': "./data/ruim_1.jpg", 'classe': 0},
-    { 'path': "./data/ruim_2.jpg", 'classe': 0},
-    { 'path': "./data/ruim_3.jpg", 'classe': 0},
-    { 'path': "./data/ruim_4.jpg", 'classe': 0}
-]
-
 def main():
     #cleanup()
     if not os.path.exists("./out"):
@@ -68,7 +58,6 @@ def cleanup():
     os.makedirs("./out")
 
 def preprocess_image(id, image_path, classe):
-
     image = cv2.imread(image_path)
     if image is None:
         print(f"Erro ao ler a imagem: {image_path}")
@@ -92,15 +81,6 @@ def preprocess_image(id, image_path, classe):
                 cropped = enhanced[y:y+h, x:x+w]
                 output_path = f"./out/{classe}-bean-{id*i}.jpg"
                 cv2.imwrite(output_path, cropped)
-
-def apply_sobel(img):
-    img_gray = img
-    grad_x = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
-    grad_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-    img_sobel = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-    return img_sobel
 
 def generate_dat_files():
     os.makedirs("./bin")
@@ -166,11 +146,3 @@ def buildMLPerceptron(treinamento, teste, treinamento_classes, teste_classes):
     print(f"Relatório de Classificação:\n{metrics.classification_report(teste_classes, tentativas)}")
 
 main()
-
-# 47 iterações, 0.652778, 150 neurônios, adam, relu
-# 27 iterações, 0.430556, 100 neurônios, adam, relu
-# 43 iterações, 0.569444, 300 neurônios, adam, relu
-# 45 iterações, 0.722222, 250 neurônios, adam, relu
-# 38 iterações, 0.694444, 200 neurônios, adam, relu
-# 100 iterações, 0.402778, 250 neurônios, sgd, relu
-# 67 iterações, 0.555556, 350 neurônios, sgd, identity
